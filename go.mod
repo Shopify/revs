4a3117f5ad9a0c7d6c746797b2426a377e295312
Chris Campbell <chris.campbell@shopify.com>
2022-05-10 12:23:54 -0600

    a browseable list with bubble tea

--- go.mod
+++ go.mod
@@ -5,15 +5,29 @@ go 1.18
 require github.com/google/go-github/v44 v44.0.0
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/charmbracelet/bubbles v0.10.3 // indirect
+	github.com/charmbracelet/bubbletea v0.20.0 // indirect
+	github.com/charmbracelet/lipgloss v0.5.0 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739 // indirect
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/exp v0.0.0-20220428152302-39d4317da171 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"fmt"
 	"os"
@@ -9,8 +8,10 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/google/go-github/v44/github"
-	"github.com/pkg/browser"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/exp/slices"
 	"golang.org/x/oauth2"
@@ -30,7 +31,65 @@ var (
 	ReasonPriority = []string{ReasonAssigned, ReasonAuthor, ReasonReviewRequested, ReasonTeamMention, ReasonMention, ReasonComment}
 )
 
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type model struct {
+	list list.Model
+}
+
+type item struct {
+	notification *github.Notification
+}
+
+func (i item) Title() string       { return *i.notification.Subject.Title }
+func (i item) Description() string { return *i.notification.Repository.FullName }
+func (i item) FilterValue() string {
+	return *i.notification.Subject.Title + " " + *i.notification.Repository.FullName
+}
+
+func initialModel(notifications []*github.Notification) model {
+
+	items := make([]list.Item, len(notifications))
+	for i, notification := range notifications {
+		items[i] = item{notification}
+	}
+
+	list := list.New(items, list.NewDefaultDelegate(), 0, 0)
+	list.Title = "Github Notifications"
+	return model{
+		list: list,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "ctrl+c" {
+			return m, tea.Quit
+		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return docStyle.Render(m.list.View())
+}
+
+var client *github.Client
+
 func main() {
+
 	ctx := context.Background()
 
 	// Authenticate with static token
@@ -40,7 +99,7 @@ func main() {
 	tc := oauth2.NewClient(ctx, ts)
 
 	// Create a new GitHub client
-	client := github.NewClient(tc)
+	client = github.NewClient(tc)
 
 	// list all notifications
 	notificationList, _, err := client.Activity.ListNotifications(ctx, nil)
@@ -57,33 +116,40 @@ func main() {
 	}
 
 	sort.Slice(notifications, func(i, j int) bool {
+		if *notifications[i].Repository.FullName != *notifications[j].Repository.FullName {
+			return *notifications[i].Repository.FullName < *notifications[j].Repository.FullName
+		}
 		return slices.Index(ReasonPriority, *notifications[i].Reason) > slices.Index(ReasonPriority, *notifications[j].Reason)
 	})
 
-	fmt.Printf("Starting PR workflow... (%d prs to review)\n", len(notifications))
-
-	for _, notification := range notifications {
-
-		id := getPullRequestID(notification)
-		merged, _, err := client.PullRequests.IsMerged(ctx, *notification.Repository.Owner.Login, *notification.Repository.Name, id)
-		if err != nil {
-			continue
-		}
-
-		if merged {
-			_, _ = client.Activity.MarkThreadRead(ctx, *notification.ID)
-			continue
-		}
-
-		prURL := getPullRequestURL(notification)
-		fmt.Println(notification.GetSubject().GetTitle())
-		fmt.Println("Opening", prURL)
-		browser.OpenURL(prURL)
-		fmt.Println("Press enter to continue...")
-		bufio.NewReader(os.Stdin).ReadBytes('\n')
-
-		_, _ = client.Activity.MarkThreadRead(ctx, *notification.ID)
+	p := tea.NewProgram(initialModel(notifications), tea.WithAltScreen())
+	if err := p.Start(); err != nil {
+		fmt.Printf("Oh no an error: %v", err)
+		os.Exit(1)
 	}
+
+	// for _, notification := range notifications {
+
+	// 	id := getPullRequestID(notification)
+	// 	merged, _, err := client.PullRequests.IsMerged(ctx, *notification.Repository.Owner.Login, *notification.Repository.Name, id)
+	// 	if err != nil {
+	// 		continue
+	// 	}
+
+	// 	if merged {
+	// 		_, _ = client.Activity.MarkThreadRead(ctx, *notification.ID)
+	// 		continue
+	// 	}
+
+	// 	prURL := getPullRequestURL(notification)
+	// 	fmt.Println(notification.GetSubject().GetTitle())
+	// 	fmt.Println("Opening", prURL)
+	// 	browser.OpenURL(prURL)
+	// 	fmt.Println("Press enter to continue...")
+	// 	bufio.NewReader(os.Stdin).ReadBytes('\n')
+
+	// 	_, _ = client.Activity.MarkThreadRead(ctx, *notification.ID)
+	// }
 }
 
 func getPullRequestURL(notification *github.Notification) string {
